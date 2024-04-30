87b170fa713197ec8c55392e415784fa668fd821
Hector Puga <pugagarciah@gmail.com>
2024-04-30 11:47:03 -0600

    Seccion 2 : 6

---
+++ hello-world/server/server.js
@@ -0,0 +1,25 @@
+
+import {ApolloServer} from '@apollo/server'
+import {startStandaloneServer} from '@apollo/server/standalone'
+const typeDefs=`#graphql
+type Query greeting{
+    greeting:String
+
+}
+
+`;
+
+const resolvers={
+
+    Query:{
+        greeting:()=>'Hola Mundo'
+    }
+
+}
+
+const server= new ApolloServer({typeDefs,resolvers});
+
+const {url}=await startStandaloneServer(server,{listen:{port:9000}});
+
+console.log(`Server running at ${url}`);
+
