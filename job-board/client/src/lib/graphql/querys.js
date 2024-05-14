203680ba6efd1027adcf05e7f4511892686148e3
Hector Puga <pugagarciah@gmail.com>
2024-05-14 17:59:42 -0600

    Sección 7 : 46

--- job-board/client/src/lib/graphql/querys.js
+++ job-board/client/src/lib/graphql/querys.js
@@ -1,6 +1,15 @@
 import { GraphQLClient, gql } from "graphql-request";
+import { getAccessToken } from '../auth';
+const client = new GraphQLClient('http://localhost:9000/graphql', {
+  headers: () => {
 
-const client = new GraphQLClient('http://localhost:9000/graphql');
+    const acessToken = getAccessToken();
+    if (acessToken) {
+      return { 'Authorization': `Bearer ${acessToken}` }
+    }
+    return {};
+  }
+});
 export async function createJob({ title, description }) {
 
   const mutation = gql`
